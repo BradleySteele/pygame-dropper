407594048dd8bc22483b4cf37c9534752435804a
Bradley <bradleysteele2000@gmail.com>
2018-06-27 11:51:07 +0100

    Added sprite collision checking

--- screen.py
+++ screen.py
@@ -22,10 +22,10 @@ class Screen:
 
         pygame.display.set_caption(title)
 
-    def show(self):
+    def show(self, game):
         if not self.running:
             self.running = True
-            self.run()
+            self.run(game)
 
     def hide(self):
         self.running = False
@@ -33,7 +33,7 @@ class Screen:
         self.surface.fill(util.colour_black)
 
     @abstractmethod
-    def run(self):
+    def run(self, game):
         """
         Called when show() is invoked and the screen is not already
         running.
@@ -59,7 +59,7 @@ class MenuScreen(Screen):
     def __init__(self, width, height):
         super(MenuScreen, self).__init__(width, height, title='Dropper | Menu')
 
-    def run(self):
+    def run(self, game):
         util.render_text(self, locale.MENU_TITLE, (200, 100), 50)
         util.render_text(self, locale.MENU_PLAY, (200, 200))
         util.render_text(self, locale.MENU_QUIT, (200, 250))
@@ -69,25 +69,35 @@ class MenuScreen(Screen):
             if sprite.text == locale.MENU_PLAY:
                 game.active_screen.hide()
                 game.active_screen = game.game_screen
-                game.active_screen.show()
+                game.active_screen.show(game)
             elif sprite.text == locale.MENU_QUIT:
                 util.exit_program()
 
 
 class GameScreen(Screen):
+    sprite_dirt = None
+    sprite_grass = None
+    sprite_player = None
 
     def __init__(self, width, height):
         super(GameScreen, self).__init__(width, height, title='Dropper | Game')
 
-    def show(self):
-        super().show()
-        util.move_rect(self, "player", (pygame.mouse.get_pos()[0], 442), util.colour_black, util.colour_cyan)
+    def show(self, game):
+        super().show(game)
+        util.move_rect(self, locale.ID_PLAYER, (pygame.mouse.get_pos()[0], 442), util.colour_black, util.colour_cyan)
 
-    def run(self):
+        for sprite in self.sprites:
+            if sprite.identifier == locale.ID_DROPPING_OBJECT:
+                if sprite.rect.colliderect(self.sprite_player.rect):
+                    print("GAME OVER")
+                elif sprite.rect.colliderect(self.sprite_dirt) or sprite.rect.colliderect(self.sprite_grass):
+                    print("HIT BOTTOM")
+
+    def run(self, game):
         self.surface.fill(util.colour_cyan)
-        util.render_rect(self, "dirt", util.colour_brown, (0, 465, 500, 35))
-        util.render_rect(self, "grass", util.colour_green, (0, 455, 500, 10))
-        util.render_rect(self, "player", util.colour_black, (230, 430, 40, 25))
+        self.sprite_dirt = util.render_rect(self, locale.ID_DIRT, util.colour_brown, (0, 465, 500, 35))
+        self.sprite_grass = util.render_rect(self, locale.ID_GRASS, util.colour_green, (0, 455, 500, 10))
+        self.sprite_player = util.render_rect(self, locale.ID_PLAYER, util.colour_black, (230, 430, 40, 25))
 
     def clicked(self, game, sprite):
         print("Clicked: " + sprite.identifier)
