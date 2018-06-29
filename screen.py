a72a587bbb98c1a9e850cb32dc189fdb5a98602b
Bradley <bradleysteele2000@gmail.com>
2018-06-29 10:23:26 +0100

    Renamed locale to dropper_locale as it clashed with unittest

--- screen.py
+++ screen.py
@@ -3,7 +3,7 @@ from random import randint
 
 import pygame
 
-import locale
+import dropper_locale
 import util
 from sprite import TextSprite
 
@@ -82,18 +82,18 @@ class MenuScreen(Screen):
         super(MenuScreen, self).__init__(width, height, title='Dropper | Menu')
 
     def run(self, game):
-        util.render_text(self, locale.MENU_TITLE, (200, 100), 50)
-        util.render_text(self, locale.MENU_PLAY, (200, 200))
-        util.render_text(self, locale.MENU_HOW_TO_PLAY, (200, 250))
-        util.render_text(self, locale.MENU_QUIT, (200, 300))
+        util.render_text(self, dropper_locale.MENU_TITLE, (200, 100), 50)
+        util.render_text(self, dropper_locale.MENU_PLAY, (200, 200))
+        util.render_text(self, dropper_locale.MENU_HOW_TO_PLAY, (200, 250))
+        util.render_text(self, dropper_locale.MENU_QUIT, (200, 300))
 
     def clicked(self, game, sprite):
         if type(sprite) == TextSprite:
-            if sprite.text == locale.MENU_PLAY:
+            if sprite.text == dropper_locale.MENU_PLAY:
                 util.display_screen(game, game.game_screen)
-            elif sprite.text == locale.MENU_HOW_TO_PLAY:
+            elif sprite.text == dropper_locale.MENU_HOW_TO_PLAY:
                 util.display_screen(game, game.how_to_play_screen)
-            elif sprite.text == locale.MENU_QUIT:
+            elif sprite.text == dropper_locale.MENU_QUIT:
                 util.exit_program()
 
 
@@ -109,16 +109,16 @@ class EndGameScreen(Screen):
         super().show(game)
 
     def run(self, game):
-        util.render_text(self, locale.END_GAME_TITLE, (200, 100), 50)
-        util.render_text(self, locale.END_GAME_SCORE.format(game.score), (200, 200), 25)
-        util.render_text(self, locale.END_GAME_HIGH_SCORE.format(game.high_score), (200, 230), 25)
-        util.render_text(self, locale.END_GAME_PLAY_AGAIN, (200, 300))
-        util.render_text(self, locale.END_GAME_RETURN_TO_MENU, (200, 350))
+        util.render_text(self, dropper_locale.END_GAME_TITLE, (200, 100), 50)
+        util.render_text(self, dropper_locale.END_GAME_SCORE.format(game.score), (200, 200), 25)
+        util.render_text(self, dropper_locale.END_GAME_HIGH_SCORE.format(game.high_score), (200, 230), 25)
+        util.render_text(self, dropper_locale.END_GAME_PLAY_AGAIN, (200, 300))
+        util.render_text(self, dropper_locale.END_GAME_RETURN_TO_MENU, (200, 350))
 
     def clicked(self, game, sprite):
-        if sprite.identifier == locale.END_GAME_PLAY_AGAIN:
+        if sprite.identifier == dropper_locale.END_GAME_PLAY_AGAIN:
             util.display_screen(game, game.game_screen)
-        elif sprite.identifier == locale.END_GAME_RETURN_TO_MENU:
+        elif sprite.identifier == dropper_locale.END_GAME_RETURN_TO_MENU:
             util.display_screen(game, game.menu_screen)
 
 
@@ -128,14 +128,14 @@ class HowToPlayScreen(Screen):
         super(HowToPlayScreen, self).__init__(width, height, title='Dropper | How to Play')
 
     def run(self, game):
-        util.render_text(self, locale.HOW_TO_PLAY_TITLE, (200, 100), 50)
-        util.render_text(self, locale.HOW_TO_PLAY_LINE_1, (200, 200), 25)
-        util.render_text(self, locale.HOW_TO_PLAY_LINE_2, (200, 225), 25)
-        util.render_text(self, locale.HOW_TO_PLAY_LINE_3, (200, 250), 25)
-        util.render_text(self, locale.HOW_TO_PLAY_GO_BACK, (200, 300), 25)
+        util.render_text(self, dropper_locale.HOW_TO_PLAY_TITLE, (200, 100), 50)
+        util.render_text(self, dropper_locale.HOW_TO_PLAY_LINE_1, (200, 200), 25)
+        util.render_text(self, dropper_locale.HOW_TO_PLAY_LINE_2, (200, 225), 25)
+        util.render_text(self, dropper_locale.HOW_TO_PLAY_LINE_3, (200, 250), 25)
+        util.render_text(self, dropper_locale.HOW_TO_PLAY_GO_BACK, (200, 300), 25)
 
     def clicked(self, game, sprite):
-        if sprite.identifier == locale.HOW_TO_PLAY_GO_BACK:
+        if sprite.identifier == dropper_locale.HOW_TO_PLAY_GO_BACK:
             util.display_screen(game, game.menu_screen)
 
 
@@ -153,10 +153,10 @@ class GameScreen(Screen):
 
     def show(self, game):
         super().show(game)
-        util.move_rect_from_id(self, locale.ID_PLAYER, (pygame.mouse.get_pos()[0], self.player_y), util.colour_black, util.colour_cyan)
+        util.move_rect_from_id(self, dropper_locale.ID_PLAYER, (pygame.mouse.get_pos()[0], self.player_y), util.colour_black, util.colour_cyan)
 
         for sprite in self.sprites:
-            if sprite.identifier == locale.ID_DROPPING_OBJECT:
+            if sprite.identifier == dropper_locale.ID_DROPPING_OBJECT:
                 if sprite.part1.colliderect(self.sprite_player.rect) or sprite.part2.colliderect(self.sprite_player.rect):
                     util.display_screen(game, game.end_game_screen)
                     return
@@ -181,10 +181,10 @@ class GameScreen(Screen):
             if gap < 5:
                 gap = 5
 
-            util.render_bar(self, locale.ID_DROPPING_OBJECT, util.colour_red, randint(0, (self.width - 100)), gap)
+            util.render_bar(self, dropper_locale.ID_DROPPING_OBJECT, util.colour_red, randint(0, (self.width - 100)), gap)
 
         pygame.draw.rect(self.surface, util.colour_cyan, self.sprite_score.rect)
-        self.sprite_score = util.render_text(self, locale.GAME_SCORE.format(game.score), (70, 20), center=False)
+        self.sprite_score = util.render_text(self, dropper_locale.GAME_SCORE.format(game.score), (70, 20), center=False)
 
     def run(self, game):
         game.score = 0
@@ -194,10 +194,10 @@ class GameScreen(Screen):
         self.surface.fill(util.colour_cyan)
         self.player_y = self.height - 70
         self.bar_hit_y = self.height - 75
-        self.sprite_dirt = util.render_rect(self, locale.ID_DIRT, util.colour_brown, (0, (self.height - 35), self.width, 35))
-        self.sprite_grass = util.render_rect(self, locale.ID_GRASS, util.colour_green, (0, (self.height - 45), self.width, 10))
-        self.sprite_player = util.render_rect(self, locale.ID_PLAYER, util.colour_black, ((self.width / 2), self.player_y, 40, 25))
-        self.sprite_score = util.render_text(self, locale.GAME_SCORE.format(game.score), (70, 20), center=False)
+        self.sprite_dirt = util.render_rect(self, dropper_locale.ID_DIRT, util.colour_brown, (0, (self.height - 35), self.width, 35))
+        self.sprite_grass = util.render_rect(self, dropper_locale.ID_GRASS, util.colour_green, (0, (self.height - 45), self.width, 10))
+        self.sprite_player = util.render_rect(self, dropper_locale.ID_PLAYER, util.colour_black, ((self.width / 2), self.player_y, 40, 25))
+        self.sprite_score = util.render_text(self, dropper_locale.GAME_SCORE.format(game.score), (70, 20), center=False)
 
     def clicked(self, game, sprite):
         pass
